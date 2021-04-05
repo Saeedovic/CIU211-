892e30aae74acac827b642a49d6cae5ecd8be0bc
Jonas Joshua Noronha <j.noronha@student.saedubai.com>
2021-04-05 21:53:23 +0400

    Added Main Menu Scene

---
+++ Assets/jonas/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Hidden room");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+}
